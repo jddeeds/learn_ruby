5d8f41cbf18fc6acf9e9ba83b6187f88c8a7d3e0
jddeeds <joey.deeds@gmail.com>
2016-08-29 23:15:23 -0500

    some answers

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add(x,y)
+	x+y
+end
+
+def subtract(x,y)
+	x-y
+end
+
+def sum(numbers)
+	total = 0
+	numbers.each do |num|
+		total += num
+	end
+	total
+end
+
+def power(a,b)
+  a ** b
+end
+
+def factorial x
+  if x <= 1
+    1
+  else
+    x * factorial(x-1)
+  end
+end
+
+def multiply(*args)
+  args.inject { |sum, n| sum * n } 
+end
